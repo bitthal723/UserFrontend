42f101bdd859933cb01ce199df74fb2bddd05ca8
bitthalChhipa723 <175926556+bitthal723@users.noreply.github.com>
2025-05-04 11:36:22 +0530

    github pages config changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/your-repo-name/',
+   base: '/UserFrontend/',
   plugins: [react()],
   server: {         // <<< You missed this server block
     port: 5174,
