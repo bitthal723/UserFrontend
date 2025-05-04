7b24b88dce57e67d4eca0028ebac2e520b9d2c7c
bitthalChhipa723 <175926556+bitthal723@users.noreply.github.com>
2025-05-04 11:32:28 +0530

    github pages config added

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/your-repo-name/',
   plugins: [react()],
   server: {         // <<< You missed this server block
     port: 5174,
     strictPort: true,
-    allowedHosts: [
-      '02e1-152-59-48-172.ngrok-free.app', // ✅ your ngrok domain
-    ],
   }
 })
